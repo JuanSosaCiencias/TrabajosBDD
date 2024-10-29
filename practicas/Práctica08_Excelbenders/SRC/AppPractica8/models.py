dacbeffe5c94a377f686848b96207ab61cf2c0c9
JSRMario <juanmario@ciencias.unam.mx>
2024-10-28 18:43:34 -0600

    feaet: agrego el inicio de la p8

--- practicas/Práctica08_Excelbenders/SRC/AppPractica8/models.py
+++ practicas/Práctica08_Excelbenders/SRC/AppPractica8/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Disciplina(models.Model):
+    iddisciplina = models.AutoField(primary_key=True)
+    nombredisciplina = models.CharField(max_length=50)
+    categoria = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'disciplina'
+        ordering = ['nombredisciplina']
+
+    def __str__(self):
+        return f"{self.iddisciplina} - {self.nombredisciplina}" 
+
+class Arbitro(models.Model):
+    idarbitro = models.AutoField(primary_key=True)
+    iddisciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE, related_name='arbitros')
+    nombre = models.CharField(max_length=50)
+    primerapellido = models.CharField(max_length=50)
+    segundoapellido = models.CharField(max_length=50)
+    fechanacimiento = models.DateField()
+    nacionalidad = models.CharField(max_length=50)
+    genero = models.CharField(max_length=1)
+    
+    class Meta:
+        db_table = 'arbitro'
+        ordering = ['nombre']
+
+
+    def __str__(self):
+        return f"{self.idarbitro} - {self.nombre}"
--- practicas/Práctica08_Excelbenders/SRC/AppPractica8/views.py
+++ practicas/Práctica08_Excelbenders/SRC/AppPractica8/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Disciplina, Arbitro
+from .serializers import DisciplinaSerializer, ArbitroSerializer
 
 # Create your views here.
+class DisciplinaAPIView(APIView):
+    
+    def get(self, request):
+        serializer = DisciplinaSerializer(Disciplina.objects.all(), many=True)
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
+    
+    # Metodos de insercion 
+
+    def post(self, request):
+        serializer = DisciplinaSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_201_CREATED, data=serializer.data)
+    
+    def put(self, request):
+        try:
+            disciplina = Disciplina.objects.get(iddisciplina=request.data['iddisciplina'])
+        except Disciplina.DoesNotExist:
+            return Response(
+                {"detail": "Disciplina no encontrada."},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        serializer = DisciplinaSerializer(Disciplina.objects.filter(iddisciplina=request.data['iddisciplina']), data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            {"detail": "Disciplina actualizada con éxito.", "data": serializer.data},
+            status=status.HTTP_200_OK
+        )
+    
+    def delete(self, request):
+        a = Disciplina.objects.filter(iddisciplina=request.data['iddisciplina']).delete()
+        return Response(status=status.HTTP_200_OK, data=a)
+ 
+class DisciplinaApiId(APIView):
+    def get(self,request):
+        try:
+            disciplina = Disciplina.objects.get(iddisciplina=request.data['iddisciplina'])
+        except Disciplina.DoesNotExist:
+            return Response(
+                {"detail": "Disciplina no encontrada."},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        serializer = DisciplinaSerializer(Disciplina.objects.filter(iddisciplina=request.data['iddisciplina']), many=True)
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
