266a53acee697435a461763518ce875368684f7e
Anthonyafk <antoniocastillo@ciencias.unam.mx>
2024-10-29 21:11:15 -0600

    Doc Models

--- practicas/Práctica08_Excelbenders/SRC/AppPractica8/models.py
+++ practicas/Práctica08_Excelbenders/SRC/AppPractica8/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 class Disciplina(models.Model):
+    """
+    Modelo que representa una disciplina.
+
+    Attributes:
+        iddisciplina (AutoField): El identificador único de la disciplina.
+        nombredisciplina (CharField): El nombre de la disciplina.
+        categoria (CharField): La categoría de la disciplina.
+    """
     iddisciplina = models.AutoField(primary_key=True)
     nombredisciplina = models.CharField(max_length=50)
     categoria = models.CharField(max_length=50)
@@ -10,9 +18,28 @@ class Disciplina(models.Model):
         ordering = ['nombredisciplina']
 
     def __str__(self):
+        """
+        Devuelve una representación en cadena del objeto Disciplina.
+
+        Returns:
+            str: Una cadena que representa la disciplina.
+        """
         return f"{self.iddisciplina} - {self.nombredisciplina}" 
 
 class Arbitro(models.Model):
+    """
+    Modelo que representa un árbitro.
+
+    Attributes:
+        idarbitro (AutoField): El identificador único del árbitro.
+        iddisciplina (ForeignKey): La disciplina a la que pertenece el árbitro.
+        nombre (CharField): El nombre del árbitro.
+        primerapellido (CharField): El primer apellido del árbitro.
+        segundoapellido (CharField): El segundo apellido del árbitro.
+        fechanacimiento (DateField): La fecha de nacimiento del árbitro.
+        nacionalidad (CharField): La nacionalidad del árbitro.
+        genero (CharField): El género del árbitro.
+    """
     idarbitro = models.AutoField(primary_key=True)
     iddisciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE, related_name='arbitros', db_column='iddisciplina')
     nombre = models.CharField(max_length=50)
@@ -26,6 +53,11 @@ class Arbitro(models.Model):
         db_table = 'arbitro'
         ordering = ['nombre']
 
-
     def __str__(self):
+        """
+        Devuelve una representación en cadena del objeto Arbitro.
+
+        Returns:
+            str: Una cadena que representa el árbitro.
+        """
         return f"{self.idarbitro} - {self.nombre}"
